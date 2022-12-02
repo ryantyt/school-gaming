c36052aa9214f9da9bb5aaf040f60c5b9b1b3b07
rtty <ryantsui786@gmail.com>
2022-12-02 13:37:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# request spammer
+
+spams requests to gaming sites so that i get flagged for school wifi
+
+no intention to dos the sites please don't arrest me
---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+import pandas as pd
+
+data = pd.read_csv('/Users/ryan/Documents/vscode/school_gaming/websites.csv')
+data = data['websites'].tolist()
+print(data)
+
+visits = 0
+
+def main():
+    # Define user agent
+    headers = {
+        "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)" 
+            "Chrome/107.0.0.0 Safari/537.36"
+    }
+    
+
+    # Get html code of the website
+    for i in data:
+        try:
+            response = requests.get(
+                url = i,
+                headers=headers
+            )
+
+            if response == '<Response [200]>':
+                response = 200
+
+            site = i.replace('https://', '')
+            print(site, response)
+            
+        except requests.exceptions.SSLError:
+            print("Bad request")
+
+        print(visits)
+
+
+if __name__ == '__main__':
+    for i in range(100):
+        main()
+        visits += 1
